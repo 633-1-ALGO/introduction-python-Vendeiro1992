a6b485a562c967411061dce6a4ac58176cfb173e
LAPTOP-9T4J2JMS\alven <elv-alex.mchdv@eduge.ch>
2019-09-20 17:08:07 +0200

    New commit

--- 12.1- Exercice - Listes et Tableaux/liste1.py
+++ 12.1- Exercice - Listes et Tableaux/liste1.py
@@ -5,4 +5,5 @@
 #                                             . .  .  .  .  .  .  .  .  .  .
 # Indication :   L'alignement rectiligne n'est pas une contrainte, tant que la table est visible ligne par ligne c'est ok.
 #               Si vous êtes perfectionnistes faites vous plaisir.
+
 liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
