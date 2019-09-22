c89a1fe2a2a8cd1d52db3397aa6d5cd60d90d212
LAPTOP-9T4J2JMS\alven <elv-alex.mchdv@eduge.ch>
2019-09-22 18:30:59 +0200

    New commit

--- 12.1- Exercice - Listes et Tableaux/liste1.py
+++ 12.1- Exercice - Listes et Tableaux/liste1.py
@@ -17,12 +17,12 @@ while i <= 1:
    print("")
    i = i + 1
 
-j = 1
-k = 1
-while k < 10:
-    print(k)
-    k += 1
+   #k = 1
+   #while k < 10:
+       #print(k)
+       #k += 1
 
+j = 1
 while j <= 10:
    nb = 1
    while nb <= 10:
