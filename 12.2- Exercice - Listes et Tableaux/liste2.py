d4839588d75692856fd0d9d03ee691bcb6eb81a9
alex.vendeiro <alex.vendeiro@etu.hesge.ch>
2019-09-23 11:26:58 +0200

    New commit

--- 12.2- Exercice - Listes et Tableaux/liste2.py
+++ 12.2- Exercice - Listes et Tableaux/liste2.py
@@ -3,8 +3,15 @@
 
 tab = [[4, 7, 3, 20, 42], [2, 4, 5, 7, 2], [23, 24, 15, 75, 23]]
 
-valMax = max(max(x) for x in tab)
-indice = [(index, row.index(valMax)) for index, row in enumerate(tab) if valMax in row]
+nb=0
+valMax = tab[0][0]
 
-print("La valeur maximum est :",valMax,"et elle se trouve à l'indice",indice)
+for i in range (len(tab)):
+    for j in range(len(tab[i])):
+        if tab[i][j] >= valMax:
+            valMax = tab[i][j]
+            indiceX = i
+            indiceY = j
+
+print("La valeur maximum est :",valMax," et elle se trouve à l'indice [ ",indiceX," ][ ",indiceY," ]")
 
